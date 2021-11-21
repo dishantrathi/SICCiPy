726eb1e51501c1d22feca5290958885c3b966c0a
Dishant Rathi <dishantrathi@users.noreply.github.com>
2021-11-21 22:22:57 +0530

    Packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SICCiPy',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:dishantrathi/SICCiPy-sdk-package.git',
+    author='Raphael Schubert',
+    author_email='raphael.schubert@digitalbankscorp.com',
+    license='unlicense',
+    packages=['SICCiPy'],
+    zip_safe=False
+)
